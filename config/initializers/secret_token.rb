c6e032f47ee19a4484c3407b24b8b431b932a093
mhopkins321 <mhopkins321@gmail.com>
2012-11-26 09:30:53 -0700

    initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ti3::Application.config.secret_token = 'f4959cfcf1837499af89e1ffc7699fd865c3943a74506bc861de9b2f5996168e48a7b8a9a931f37628d578499bdb68471cdc8f363d8898ff58b2b18fb75ed2ad'
